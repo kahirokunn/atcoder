646253105135141aff1ccec01bf3f4b33afb3e8c
k-okina <okinakahiro@gmail.com>
2019-05-23 01:19:37 +0900

    [retry] ABC085C - Otoshidama

---
+++ retry/abc085_c.rs
@@ -0,0 +1,30 @@
+use std::io::*;
+use std::str::FromStr;
+
+fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().expect("failed to parse token")
+}
+
+fn main() {
+    let n = read::<u32>();
+    let y = read::<u32>();
+
+    for i in 0..n + 1 {
+        for j in 0..n + 1 - i {
+            let k = n - i - j;
+            if i * 10000 + j * 5000 + k * 1000 == y {
+                println!("{} {} {}", i, j, k);
+                return;
+            }
+        }
+    }
+    println!("-1 -1 -1");
+}
