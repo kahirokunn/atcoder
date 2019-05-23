b2d09e59afdf50eb23bd96a0dfa6ea616a1be265
k-okina <okinakahiro@gmail.com>
2019-05-24 00:49:41 +0900

    [add] よりシンプルに A - キャンディーと2人の子供 / Fighting over Candies を解いてみた

---
+++ retry/abc047_a.rs
@@ -0,0 +1,39 @@
+use std::io::*;
+use std::str::FromStr;
+
+fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().expect("failed to parse token")
+}
+
+fn read_n<T: FromStr>(n: u32) -> Vec<T> {
+    read_n_logic::<T>(n, vec![])
+}
+
+fn read_n_logic<T: FromStr>(n: u32, mut a: Vec<T>) -> Vec<T> {
+    match n {
+        0 => a,
+        _ => {
+            a.push(read::<T>());
+            read_n_logic(n - 1, a)
+        }
+    }
+}
+
+fn main() {
+    let mut c = read_n::<u32>(3);
+    c.sort();
+    let largest = c.pop().unwrap();
+
+    match largest == c.pop().unwrap() + c.pop().unwrap() {
+        true => println!("Yes"),
+        false => println!("No"),
+    };
+}
